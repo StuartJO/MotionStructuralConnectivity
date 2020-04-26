50b0598ab1f4dcb8a8d140b4b56705bc80f6a9b7
StuartJO <sjold1@student.monash.edu>
2020-04-26 23:14:53 +1000

    Added comments
    Added comments describing what each analysis does

--- MAIN_ANALYSIS.m
+++ MAIN_ANALYSIS.m
@@ -5,12 +5,20 @@ PATH = pwd;
 
 addpath(genpath(PATH))
 
+% This calculates the main data (QCSC with ABSall at a .05 edge consistency threshold) reported in the paper
+
 CalculateQCSC(1,.05,1,0,1,'./analysed_data');
 
+% This calculates the main data (QCSC with ABSall) with no threshold applied
+
 CalculateQCSC(1,0,0,1,1,'./analysed_data')
+
+% This calculates QCSC with different motion measures
 
 for i = 2:7
     CalculateQCSC(i,.05,0,0,0,'./analysed_data');
 end
+
+% This calculates QCSC for EDDY1.5
 
 CalculateQCSC_EDDY(.05,0,'./analysed_data');
